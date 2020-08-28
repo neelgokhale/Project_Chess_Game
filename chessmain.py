bd070712e9aaa97c4ba7cde26d536d042a062d68
Neel Gokhale <neelg14@gmail.com>
2020-08-27 22:26:47 -0400

    added move highlighting + animations
    Signed-off-by: Neel Gokhale <neelg14@gmail.com>

--- chessmain.py
+++ chessmain.py
@@ -42,7 +42,9 @@ def main():
     gs = chessengine.GameState()
     valid_moves = gs.get_valid_moves()
     move_made = False  # flag var for when a valid move is made by user
-    animate = False # flag var for enabling anims
+    animate = False  # flag var for enabling anims
+    game_over = False  # game over
+
     load_images()
 
     running = True
@@ -57,33 +59,41 @@ def main():
                 running = False
 
             elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                col = location[0] // SQ_SIZE
-                row = location[1] // SQ_SIZE
+                if not game_over:
+                    location = p.mouse.get_pos()
+                    col = location[0] // SQ_SIZE
+                    row = location[1] // SQ_SIZE
 
-                if sq_selected == (row, col):
-                    sq_selected = ()
-                    player_click = []
-                else:
-                    sq_selected = (row, col)
-                    player_click.append(sq_selected)
-                if len(player_click) == 2:
-                    move = chessengine.Move(player_click[0], player_click[1], gs.board)
-                    print(move.get_chess_notation())
-                    for i in range(len(valid_moves)):
-                        if move == valid_moves[i]:
-                            gs.make_move(valid_moves[i])
-                            move_made = True
-                            animate = True
-                            sq_selected = ()
-                            player_click = []
-                    if not move_made:
-                        player_click = [sq_selected]
+                    if sq_selected == (row, col):
+                        sq_selected = ()
+                        player_click = []
+                    else:
+                        sq_selected = (row, col)
+                        player_click.append(sq_selected)
+                    if len(player_click) == 2:
+                        move = chessengine.Move(player_click[0], player_click[1], gs.board)
+                        print(move.get_chess_notation())
+                        for i in range(len(valid_moves)):
+                            if move == valid_moves[i]:
+                                gs.make_move(valid_moves[i])
+                                move_made = True
+                                animate = True
+                                sq_selected = ()
+                                player_click = []
+                        if not move_made:
+                            player_click = [sq_selected]
 
             elif e.type == p.KEYDOWN:
-                if e.key == p.K_z:
+                if e.key == p.K_z:  # undo
                     gs.undo_move()
                     move_made = True
+                    animate = False
+                if e.key == p.K_r:  # reset
+                    gs = chessengine.GameState()
+                    valid_moves = gs.get_valid_moves()
+                    sq_selected = ()
+                    player_click = []
+                    move_made = False
                     animate = False
 
         if move_made:
@@ -94,6 +104,17 @@ def main():
             animate = False
 
         draw_game_state(screen, gs, valid_moves, sq_selected)
+
+        if gs.check_mate:
+            game_over = True
+            if gs.whitetomove:
+                draw_text(screen, "Black wins by checkmate")
+            else:
+                draw_text(screen, "White wins by checkmate")
+        elif gs.stale_mate:
+            game_over = True
+            draw_text(screen, "Stalemate")
+
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -153,11 +174,11 @@ def animate_move(move, screen, board, clock):
     global colors
     dR = move.end_row - move.start_row
     dC = move.end_col - move.start_col
-    frames_per_sq = 10 # frames per square
+    frames_per_sq = 10  # frames per square
     frame_count = (abs(dR) + abs(dC)) * frames_per_sq
     for frame in range(frame_count + 1):
         r, c = ((move.start_row + dR * (frame / frame_count),
-                       move.start_col + dC * (frame / frame_count)))
+                 move.start_col + dC * (frame / frame_count)))
         draw_board(screen)
         draw_pieces(screen, board)
         # erase piece moved from ending square
@@ -173,23 +194,15 @@ def animate_move(move, screen, board, clock):
         clock.tick(60)
 
 
+def draw_text(screen, text):
+    font = p.font.SysFont("Arial", 32, True, False)
+    text_obj = font.render(text, 0, p.Color('white'))
+    text_location = p.Rect(0, 0, WIDTH, HEIGHT).move(WIDTH / 2 - text_obj.get_width() / 2,
+                                                     HEIGHT / 2 - text_obj.get_height() / 2)
+    screen.blit(text_obj, text_location)
+    text_obj = font.render(text, 0, p.Color('black'))
+    screen.blit(text_obj, text_location.move(2, 2))
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
